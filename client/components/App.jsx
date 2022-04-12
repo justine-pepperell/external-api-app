72e98d7a34e3041b0b55c23340e7eb0453bddb34
Jack Gordon <jackgordon@Trantham-2.local>
2022-04-13 11:55:58 +1200

    initial

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,44 @@
 import React, { useState, useEffect } from 'react'
 
+import { getSpacePicture } from '../api/nasa'
+
 import { getWelcome } from '../api'
 
 function App() {
   const [welcomeStatement, setWelcomeStatement] = useState('')
+  const [spaceObj, setSpaceObj] = useState(null)
 
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-        return null
+  const findSpace = () => {
+    getSpacePicture()
+      .then(obj => {
+        setSpaceObj(obj)
       })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  })
+  }
 
-  return <h1>{welcomeStatement}</h1>
+
+
+
+
+
+  // useEffect(() => {
+  //   getWelcome()
+  //     .then((res) => {
+  //       setWelcomeStatement(res.statement)
+  //       return null
+  //     })
+  //     .catch((err) => {
+  //       console.error(err.message)
+  //     })
+  // })
+
+  return (
+    <>
+      <h1>NASA space is crazy aye!</h1>
+      <button onClick={findSpace}>Get space pic</button>
+      {/* {spaceObj && <img src={spaceObj.url} />} */}
+      <img src={spaceObj?.url} />
+    </>
+  )
 }
 
 export default App

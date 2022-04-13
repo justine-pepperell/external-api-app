1cc5aa72b51edfa16c3e379461e4c624509d063d
Seb Ninness <97018698+seb-ninness@users.noreply.github.com>
2022-04-13 12:37:58 +1200

    first external api added

--- client/api.js
+++ client/api.js
@@ -1,9 +1,16 @@
 import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
+const cocktailURL = 'www.thecocktaildb.com/api/json/v1/1/'
 
-// *** EXAMPLE ***
 export function getWelcome() {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
-// ***   ***   ***
+
+export function getRandomDriver() {
+  return request
+    .get('https://mario-kart-tour-api.herokuapp.com/api/v1/drivers')
+    .then((resp) => {
+      return resp.body
+    })
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome } from '../api'
+import { getWelcome , getRandomDriver } from '../api'
 
 function App() {
   const [welcomeStatement, setWelcomeStatement] = useState('')
+  const [getDriver, setGetDriver] = useState(null)
 
   useEffect(() => {
     getWelcome()
@@ -16,7 +17,28 @@ function App() {
       })
   })
 
-  return <h1>{welcomeStatement}</h1>
+  const getADriver = () => {
+    getRandomDriver()
+      .then(arr => {
+        const singleDriverObj = arr[Math.floor(Math.random() * 120)]
+        console.log(singleDriverObj)
+        setGetDriver(singleDriverObj)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }
+
+
+  return (
+  <div className='main-container'>
+    <h1>{welcomeStatement}</h1>
+    <button onClick={getADriver}>Get a random Driver!</button>
+    <h3>The driver you have got is: {getDriver?.name}</h3>
+    <h4>They have the special skill: {getDriver?.special_skill}</h4>
+    <h4>They are this rare: {getDriver?.rarity}</h4>
+  </div>
+    )
 }
 
 export default App

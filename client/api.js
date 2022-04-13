cda5eccde3e8ee2e2b168467ce71e35862e20a75
Lucia Kennedy <97010972+lucia-kennedy@users.noreply.github.com>
2022-04-13 16:42:22 +1200

    get a random ghibli film and species

--- client/api.js
+++ client/api.js
@@ -1,9 +1,31 @@
 import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
+const ghibliURL = 'https://ghibliapi.herokuapp.com'
+
 
 // *** EXAMPLE ***
 export function getWelcome() {
   return request.get(`${serverURL}/welcome`).then((response) => response.body)
 }
 // ***   ***   ***
+
+
+export function getGhibli () {
+  return request 
+  .get(ghibliURL + '/species')
+  .then(resp => {
+    console.log(resp.body)
+    return resp.body
+  })
+}
+
+export function getFilm () {
+  return request 
+  .get(ghibliURL + '/films')
+  .then(resp => {
+    console.log(resp.body)
+    return resp.body
+  })
+
+}
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,22 +1,72 @@
 import React, { useState, useEffect } from 'react'
 
-import { getWelcome } from '../api'
+import { getGhibli, getFilm } from '../api'
+
 
 function App() {
-  const [welcomeStatement, setWelcomeStatement] = useState('')
+  // const [welcomeStatement, setWelcomeStatement] = useState('')
+  const [ghibliSpec, setGhibliSpec] = useState([])
+  const [getAFilm, setAFilm] = useState([])
 
-  useEffect(() => {
-    getWelcome()
-      .then((res) => {
-        setWelcomeStatement(res.statement)
-        return null
+ 
+
+  const findGhibliSpec = () => {
+    getGhibli()
+      .then(ghibliArr => {
+        const ghibliObj = ghibliArr[Math.floor(Math.random() *ghibliArr.length)]
+        console.log(ghibliObj)
+        setGhibliSpec(ghibliObj)
       })
       .catch((err) => {
         console.error(err.message)
       })
-  })
+  }
 
-  return <h1>{welcomeStatement}</h1>
+  const findFilm = () => {
+    getFilm()
+      .then(filmArr => {
+        const filmObj = filmArr[Math.floor(Math.random() *filmArr.length)]
+        console.log(filmObj)
+        setAFilm(filmObj)
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }
+
+  return (
+<>
+  {/* <h1>{welcomeStatement}</h1> */}
+  
+<button onClick={findFilm}>Get A Ghibli Film</button>
+    <h1>Title: {getAFilm?.title}</h1>
+    <h2>Original Title: {getAFilm?.original_title}</h2>
+    <h2>Description: {getAFilm?.description}</h2>
+    <h2>Director: {getAFilm?.director}</h2>
+    <h2>Release Date: {getAFilm.release_date}</h2>
+    <h2>Running Time: {getAFilm?.running_time}</h2>
+    <h2>Rating: {getAFilm?.rt_score}</h2>
+
+
+
+
+
+  <button onClick={findGhibliSpec}>Get Ghibli Species</button>
+    <h1>Name: {ghibliSpec?.name}</h1>
+    <h2>Classification: {ghibliSpec?.classification}</h2>
+    <h2>Eye Colour: {ghibliSpec?.eye_colors}</h2>
+    <h2>Hair Colour: {ghibliSpec?.hair_colors}</h2>
+    <h2>URL: {ghibliSpec.url}</h2>
+    <h2>People: {ghibliSpec?.people}</h2>
+    <h2>Films: {ghibliSpec?.films}</h2>
+
+    
+    
+  
+  
+  
+</> 
+)
 }
 
 export default App

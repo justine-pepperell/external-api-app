14f3d1d75179c1e7c375518c8150bb0f7948af10
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-04-13 17:00:43 +1200

    shows recipe and info

--- client/components/Recipe.jsx
+++ client/components/Recipe.jsx
@@ -4,23 +4,39 @@ import { getRecipe } from '../recipe'
 function RandRecipe() {
 
   const [recipe, setRecipe] = useState(null)
+  const [recipeVisible, setRecipeVisible] = useState(false)
 
   function findRecipe() {
     getRecipe()
     .then((randRecipe) => {
       setRecipe(randRecipe)
+      setRecipeVisible(true)
     })
     .catch ((err) => {alert(err.messsage)})
   }
 
-
-
   return (
-    <p>
-      <button onClick={findRecipe}>Give me a recipe!</button>
-      {recipe && recipe.recipes.map((each) => {each})}
-      
-    </p>
+    <>
+      <button onClick={findRecipe}>{recipeVisible ? 'Another One!' : 'Give me a recipe!'}</button>
+
+      <a href={recipe && recipe.recipes[0].sourceUrl}><h2>{recipe && recipe.recipes[0].title}</h2></a>
+
+      <img src={recipe && recipe.recipes[0].image} />
+      <br>
+      </br>
+      <> 
+        {recipe && 'Things you will need are:'} 
+        <br></br>
+        <ul>
+        {recipe && recipe.recipes[0].extendedIngredients.map((ingred) => 
+          <li key={ingred.id}>{ingred.original}</li>
+        )}
+        </ul>
+      </>
+      {recipe && 'Instructions:'}
+      <br></br>
+      {recipe && recipe.recipes[0].instructions.replace('</li><li>', '').replace('</ol>', '').replace('<ol>', '')}
+    </>
   )
 }
 
